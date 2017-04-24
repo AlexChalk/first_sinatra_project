3ba38b605d9ff160d2e7bc9f021028f5057e63f5
Alex Chalk <alexchalk17@gmail.com>
2017-04-24 11:53:50 -0400

    Add \named-cat page

--- app.rb
+++ app.rb
@@ -15,11 +15,17 @@ get '/elephants' do
   "I sympathise, and I wish I had an elephant or two to give you."
 end
 
-get '/harry_potter' do
+get '/harry-potter' do
   "Get him, take him to the dark lord!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_names = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_names = params[:name]
   erb(:index)
 end
